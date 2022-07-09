54de7698ac97f7bb89b8a9fce7fd485196c8e0be
Breno Silva <brenoqn2@gmail.com>
2022-07-09 02:52:59 -0300

    prepare for deploy

--- src/database.ts
+++ src/database.ts
@@ -5,6 +5,9 @@ dotenv.config();
 const { Pool } = pg;
 export const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 connection.query("SELECT NOW()", (err, res) => {
